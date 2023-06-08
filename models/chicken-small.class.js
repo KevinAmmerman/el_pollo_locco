5178ad06acfde346d576d57f1a9473b88408ae30
Kevin Ammerman <127098309+KevinAmmerman@users.noreply.github.com>
2023-06-08 12:32:37 +0200

    implementing collision for chicken

--- models/chicken-small.class.js
+++ models/chicken-small.class.js
@@ -7,22 +7,29 @@ class ChickenSmall extends MovableObject {
         'img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
         'img/3_enemies_chicken/chicken_small/1_walk/3_w.png'
     ]
+    IMAGE_DEAD = 'img/3_enemies_chicken/chicken_small/2_dead/dead.png';
+    energy = 1;
 
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
-        this.x = 200 + Math.random() * 500;
+        this.x = 200 + Math.random() * 1800;
         this.speed = 0.15 + Math.random() * 0.25;
-        this.loadImages(this.IMAGES_WALKING); 
-        this.animate(); 
+        this.loadImages(this.IMAGES_WALKING);
+        this.animate();
+        this.isDead();
     }
 
-    
-    animate()  {
-        setInterval(() => {
+
+    animate() {
+        const moveInterval = setInterval(() => {
             this.moveLeft(); // lets the chicken move to the left
         }, 1000 / 60);
-        setInterval(() => {
+        const animateInterval = setInterval(() => {
             this.playAnimation(this.IMAGES_WALKING);
         }, 200);
+
+        if(this.enery == 0) {
+            
+        }
     }
 }
